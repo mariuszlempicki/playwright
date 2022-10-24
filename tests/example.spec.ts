3a3f88c2f3f3287c7708f9e1bcedbddee63cade0
MariuszLempicki <mlempicki@doz.pl>
2022-10-24 23:53:57 +0200

    test mod

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,13 @@
 import { test, expect } from '@playwright/test';
-
+const config: test = {
+  use: {
+    video: {
+      mode: 'on-first-retry',
+      size: { width: 640, height: 480 }
+    }
+  },
+};
+export default config;
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.locator('text=Get started').click();
